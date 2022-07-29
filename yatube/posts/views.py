07272653a9d90bacdd5efc0d122cca35ae3a0048
Артём Гончаров <tenagr41@gmail.com>
2022-07-29 08:12:05 +0300

    Yatube v.1.6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,10 +6,7 @@ POSTS_COUNT: int = 10
 
 
 def index(request):
-    # подставил select_related(group), тесты выдают ошибку
-    # как я понимаю это функция главной страницы здесть должны быть все посты
-    # а не объедененные по группам
-    posts = Post.objects.select_related()[:POSTS_COUNT]
+    posts = Post.objects.select_related('group')[:POSTS_COUNT]
     context = {
         'posts': posts
     }
@@ -18,7 +15,6 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    # related_name указал, но не очень понял как реализовать
     posts = group.group_post.all()[:POSTS_COUNT]
     context = {
         'group': group,
